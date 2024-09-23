c743de218d92743384e3c4da0ac4a2936bba68fa
LucasNagasse <lucas.nagasse@sptech.school>
2024-09-23 14:30:34 -0300

    atualização backend

--- web-data-viz-main/app.js
+++ web-data-viz-main/app.js
@@ -1,33 +1,35 @@
 // dependências:
+//   importação da framework "express":
 const express = require("express");
+//   importação do middleware "cors":
 const cors = require("cors");
+//   importação da biblioteca "path":
 const path = require("path");
 
+//   importação das variáveis de ambiente (.env):
 // const ambiente_processo = 'producao';
-const ambiente_processo = 'desenvolvimento';
+const ambiente_processo = "desenvolvimento";
 
-const caminho_env = ambiente_processo === 'producao' ? '.env' : '.env.dev';
+const caminho_env = ambiente_processo === "producao" ? ".env" : ".env.dev";
 require("dotenv").config({ path: caminho_env });
 const PORTA_APP = process.env.APP_PORT;
 const HOST_APP = process.env.APP_HOST;
 
+//   importação das rotas:
+const usuarioRouter = require("./src/routers/usuarioRouter.js");
+
 // aplicação:
 const app = express();
 
-// dependências de rotas:
-const indexRouter = require("./src/routers/indexRouter.js");
-const usuarioRouter = require("./src/routers/usuarioRouter.js");
-
-// configurações:
+//   configurações:
 app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 
 //   configuração das rotas:
-app.use("/", indexRouter);
 app.use("/usuario", usuarioRouter);
 
-// aplicação rodando:
+//   início da aplicação:
 app.listen(PORTA_APP, function () {
-    console.log(`Servidor rodando em http://127.0.0.1:${PORTA_APP}`);
+  console.log(`Servidor rodando em http://127.0.0.1:${PORTA_APP}`);
 });
--- web-data-viz-main/src/routers/usuarioRouter.js
+++ web-data-viz-main/src/routers/usuarioRouter.js
@@ -1,15 +1,22 @@
-var express = require("express");
-var router = express.Router();
+// dependências:
+//   importação da framework "express":
+const express = require("express");
 
-var usuarioController = require("../controllers/usuarioController");
+//   importação do usuarioController.js:
+const usuarioController = require("../controllers/usuarioController");
 
-//Recebendo os dados do html e direcionando para a função cadastrar de usuarioController.js
+// variável do roteador:
+const router = express.Router();
+
+// envia a requisição do tipo post /cadastrar para o controller:
 router.post("/cadastrar", function (req, res) {
-    usuarioController.cadastrar(req, res);
-})
-
-router.post("/autenticar", function (req, res) {
-    usuarioController.autenticar(req, res);
+  usuarioController.cadastrar(req, res);
 });
 
+// envia a requisição do tipo post /autenticar para o controller:
+router.post("/autenticar", function (req, res) {
+  usuarioController.autenticar(req, res);
+});
+
+// exporta o roteador:
 module.exports = router;
