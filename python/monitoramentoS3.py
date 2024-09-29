b03ab3dc27d80e147e7fee139cd6401607049ac7
LucasNagasse <lucas.nagasse@sptech.school>
2024-09-29 10:34:27 -0300

    atualização no monitoramentoS3.py

--- python/monitoramentoS3.py
+++ python/monitoramentoS3.py
@@ -1,8 +1,9 @@
-import csv
-import psutil
-from uuid import getnode
-import boto3
 import time
+from uuid import getnode
+import csv
+
+import psutil
+import boto3
 
 contagem = 0
 
@@ -12,14 +13,11 @@ while (True):
     cpuPorcentagem = psutil.cpu_percent()
     ramPorcentagem = psutil.virtual_memory().percent
     data.append({"mac_address": hex(getnode())[2:], "cpu_porcentagem": cpuPorcentagem, "ram_porcentagem": ramPorcentagem})
+    time.sleep(5)
 
   with open('registros.csv', 'w', newline='') as csvfile:
-    fieldnames = ['mac_address', 'cpu_porcentagem', 'ram_porcentagem']
-    writer = csv.DictWriter(csvfile, fieldnames = fieldnames)
+    writer = csv.DictWriter(csvfile, fieldnames = ['mac_address', 'cpu_porcentagem', 'ram_porcentagem'])
     writer.writeheader()
     writer.writerows(data)
   
-  s3 = boto3.resource('s3')
-  s3.Bucket("s3-raw-mktsf").upload_file("registros.csv", "registros" + str(contagem) + ".csv")
-
-  time.sleep(5)
+  boto3.resource('s3').Bucket("s3-raw-mktsf").upload_file("registros.csv", "registros" + str(contagem) + ".csv")
