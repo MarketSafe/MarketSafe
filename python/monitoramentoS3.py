cc13e8f16cb286f8c8a1ec84a3fb9fe70c71b826
LucasNagasse <lucas.nagasse@sptech.school>
2024-10-01 23:48:54 -0300

    atualização monitoramentoS3.py

--- python/monitoramentoS3.py
+++ python/monitoramentoS3.py
@@ -5,19 +5,48 @@ import csv
 import psutil
 import boto3
 
+accessKeyId = input("Insira o id da sua chave de acesso: ")
+secretAccessKey = input("Insira a sua chave de acesso secreta: ")
+sessionToken = input("Insira o seu token de sessão: ")
+
+prints = ""
+while (True):
+  prints = input("Deseja exibir prints da execução ? (s/n; default: n): ")
+  if (prints == ""): prints = "n"
+  if (prints in ["s", "n"]):
+    break
+  print("Inválido, tente novamente")
+
+if (prints == "s"): print("Monitoramento iniciado.")
+
 contagem = 0
 
 while (True):
-  data = []
-  for _ in range(10):
+  dados = []
+  
+  for i in range(10):
     cpuPorcentagem = psutil.cpu_percent()
     ramPorcentagem = psutil.virtual_memory().percent
-    data.append({"mac_address": hex(getnode())[2:], "cpu_porcentagem": cpuPorcentagem, "ram_porcentagem": ramPorcentagem})
-    time.sleep(5)
+    dados.append({
+      "mac_address": hex(getnode())[2:],
+      "cpu_porcentagem": cpuPorcentagem,
+      "ram_porcentagem": ramPorcentagem
+    })
+    
+    if (prints == "s"): print(str(i + 1) + " dado(s) lido(s)...")
+    time.sleep(1)
 
-  with open('registros.csv', 'w', newline='') as csvfile:
-    writer = csv.DictWriter(csvfile, fieldnames = ['mac_address', 'cpu_porcentagem', 'ram_porcentagem'])
-    writer.writeheader()
-    writer.writerows(data)
+  csvName = "registro." + str(contagem) + "." + hex(getnode())[2:] + ".csv"
   
-  boto3.resource('s3').Bucket("s3-raw-mktsf").upload_file("registros.csv", "registros" + str(contagem) + ".csv")
+  with open("registro0.csv", "w", newline="") as csvfile:
+    writer = csv.DictWriter(csvfile, fieldnames = ["mac_address", "cpu_porcentagem", "ram_porcentagem"])
+    writer.writeheader()
+    writer.writerows(dados)
+  
+  # s3 = boto3.client(service_name = "s3", region_name = "us-east-1", aws_access_key_id = accessKeyId, aws_secret_access_key = secretAccessKey, aws_session_token = sessionToken)
+  
+  # s3.upload_file("registro.csv", "s3-raw-mkts", csvName)
+  
+  if (prints == "s"): print("\"" + csvName + "\" enviado !")
+  
+  contagem += 1
