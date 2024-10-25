8d59fafea34aa1130f1914fc3dc288001254f6f1
LucasNagasse <lucas.nagasse@sptech.school>
2024-10-25 00:40:03 -0300

    pequena alteração no banco de dados

--- web-data-viz-main/src/database/insert.sql
+++ web-data-viz-main/src/database/insert.sql
@@ -8,7 +8,7 @@ insert into empresa (razao_social, nome_fantasia, cnpj, email, telefone, fk_ende
 
 insert into funcionario (nome, cpf, cargo, email, senha, telefone, fk_empresa) values
   ("Gisele", "123.456.789-01", "gerente", "gisele@gmail.com", "123456789", "+12 (34) 56789-0123", 1),
-  ("Nagasse", "123.456.789-02", "técnico", "nagasse@gmail.com", "123456789", "+12 (34) 56789-0456", 1);
+  ("Nagasse", "123.456.789-02", "analista", "nagasse@gmail.com", "123456789", "+12 (34) 56789-0456", 1);
 
 insert into filial (fk_empresa, fk_endereco) values
   (1, 1);
