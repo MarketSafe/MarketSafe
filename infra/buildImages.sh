edc8edd57bc8f2c3264e162fadc34fbc88ca37d0
giselerezende2 <gisele.carvalho@sptech.school>
2024-11-12 14:35:55 -0300

    Alterações building img

--- infra/buildImages.sh
+++ infra/buildImages.sh
@@ -1,7 +1,7 @@
 # site (rodar no MarketSafe/site/)
-sudo docker build -t testesite
+sudo docker build -t testesite .
 sudo docker image tag testesite giselerezende/mkts-gisele:site
 
 # db (rodar no MarketSafe/site/src/database/)
-sudo docker build -t testedb
+sudo docker build -t testedb .
 sudo docker image tag testedb giselerezende/mkts-gisele:db
