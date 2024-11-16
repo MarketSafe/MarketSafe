424f2ed6ed2a87630cf5776792fff38f5b194105
LucasNagasse <lucas.nagasse@sptech.school>
2024-11-16 04:55:46 -0300

    alterações nos comentários do backend

--- site/src/models/alertaModel.js
+++ site/src/models/alertaModel.js
@@ -1,25 +1,25 @@
 // alertaModel.js:
 
 // dependências:
-//   importação do arquivo `config.js`:
+//   importa o arquivo `config.js`:
 const database = require("../database/config.js");
 
-// declaração da função `listarPorFilial`:
+// declara a função `listarPorFilial`:
 function listarPorFilial(fk_filial) {
-  // declaração da variável de instrução sql:
+  // declara a variável de instrução sql:
   const instrucao = `select a.id as alertaId, a.data_hora as alertaDataHora, a.cpu_porcentagem as alertaCpuPorcentagem, a.ram_porcentagem as alertaRamPorcentagem, t.mac_address as totemMacAddress from alerta as a join totem as t on a.fk_totem = t.id where t.fk_filial = '${fk_filial}';`;
-  // declaração da variável de resultado da execução:
+  // declara a variável de resultado da execução:
   const resultado = database.executar(instrucao);
   // retorna o resultado da execução:
   return resultado;
 }
 
-// declaração da função `listarPorEmpresa`:
+// declara a função `listarPorEmpresa`:
 function listarPorEmpresa(fk_empresa) {
-  // declaração da variável de instrução sql:
+  // declara a variável de instrução sql:
   const instrucao = `select a.id as alertaId, a.data_hora as alertaDataHora, a.cpu_porcentagem as alertaCpuPorcentagem, a.ram_porcentagem as alertaRamPorcentagem, t.mac_address as totemMacAddress, f.id as filialId from alerta as a join totem as t on a.fk_totem = t.id join filial as f on t.fk_filial = f.id where f.fk_empresa = '${fk_empresa}';`;
   console.log(instrucao);
-  // declaração da variável de resultado da execução:
+  // declara a variável de resultado da execução:
   const resultado = database.executar(instrucao);
   // retorna o resultado da execução:
   return resultado;
--- site/src/models/totemModel.js
+++ site/src/models/totemModel.js
@@ -1,34 +1,34 @@
 // totemModel.js:
 
 // dependências:
-//   importação do arquivo `config.js`:
+//   importa o arquivo `config.js`:
 const database = require("../database/config.js");
 
-// declaração da função `cadastrar`:
+// declara a função `cadastrar`:
 function cadastrar(mac_address, fk_filial) {
-  // declaração da variável de instrução sql:
+  // declara a variável de instrução sql:
   const instrucao = `insert into totem (mac_address, fk_filial) values ('${mac_address}', '${fk_filial}');`;
-  // declaração da variável de resultado da execução:
+  // declara a variável de resultado da execução:
   const resultado = database.executar(instrucao);
   // retorna o resultado da execução:
   return resultado;
 }
 
-// declaração da função `listar`:
+// declara a função `listar`:
 function listarPorFilial(fk_filial) {
-  // declaração da variável de instrução sql:
+  // declara a variável de instrução sql:
   const instrucao = `select id, data_hora, mac_address from totem where fk_filial = '${fk_filial}';`;
-  // declaração da variável de resultado da execução:
+  // declara a variável de resultado da execução:
   const resultado = database.executar(instrucao);
   // retorna o resultado da execução:
   return resultado;
 }
 
-// declaração da função `listar`:
+// declara a função `listar`:
 function listarPorEmpresa(fk_empresa) {
-  // declaração da variável de instrução sql:
+  // declara a variável de instrução sql:
   const instrucao = `select id, data_hora, mac_address, fk_filial from totem where fk_empresa = '${fk_empresa}';`;
-  // declaração da variável de resultado da execução:
+  // declara a variável de resultado da execução:
   const resultado = database.executar(instrucao);
   // retorna o resultado da execução:
   return resultado;
