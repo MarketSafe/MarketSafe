73e4ff4c939537e4ce4cf56c475d5785e610d6fc
LucasLucena31 <lucas.lucena@sptech.school>
2024-11-18 10:14:47 -0300

    adicionando select pra contar alertas por dia da semana

--- site/src/models/totemModel.js
+++ site/src/models/totemModel.js
@@ -43,7 +43,34 @@ function listarAlertaPorTotem(totem, componente, inicio, fim) {
   // declaração da variável de resultado da execução:
   const resultado = database.executar(instrucao);
   // retorna o resultado da execução:
-  return resultado;
+
+  // declaração da variável de instrução sql:
+  const instrucao2 = `SELECT 
+    dias_semana.dia AS DIA_SEMANA,
+    COALESCE(COUNT(alerta.id), 0) AS qtd
+FROM 
+    (SELECT 1 AS dia UNION ALL
+     SELECT 2 UNION ALL
+     SELECT 3 UNION ALL
+     SELECT 4 UNION ALL
+     SELECT 5 UNION ALL
+     SELECT 6 UNION ALL
+     SELECT 7) AS dias_semana
+LEFT JOIN 
+    alerta ON DATE_FORMAT(alerta.data_hora, '%w') = dias_semana.dia
+GROUP BY 
+    dias_semana.dia
+ORDER BY 
+    FIELD(dias_semana.dia, 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday');`;
+  // declaração da variável de resultado da execução:
+  const resultado2 = database.executar(instrucao2);
+
+  const resultadoFinal = {
+    resultado,
+    resultado2
+  }
+  // retorna o resultado da execução:
+  return resultadoFinal;
 }
 
 
