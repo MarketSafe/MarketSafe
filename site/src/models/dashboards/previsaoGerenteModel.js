62dce0a57872c78823916c384cc9638c2835dc22
Enzo-Jorge <enzo.nascimento@sptech.school>
2024-12-03 18:15:09 -0300

    Terminando o gráfico de bubble e arrumando o background.svg na dash de previsão do gerente

--- site/src/models/dashboards/previsaoGerenteModel.js
+++ site/src/models/dashboards/previsaoGerenteModel.js
@@ -33,29 +33,18 @@ function puxarDadosBubble(idFilial, idEmpresa, anoMes, idPromocao) {
     console.log("ACESSEI O PREVISÃO GERENTE MODEL \n \n\t\t >> Se aqui der erro de 'Error; connect ECONNREFUSED', \n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD esta rodando corretamente. \n\n function puxarDadosBubble()");
 
     var instrucaoSql = `
-    select
-  f.*,
-  sum(t.quantidade_alerta) quantidade_alerta
-  from filial f
-  left join (
-        select
-        t.*,
-        ifnull(a.quantidade_alerta, 0) quantidade_alerta
-        from totem t
-        left join (
-            select
-            count(a.id) quantidade_alerta,
-            a.fk_totem
-            from alerta a
-            where date(a.data_hora) = date("${anoMes}-15")
-             and a.fk_promocao = ${idPromocao}
-            group by a.fk_totem
-        ) a
-            on t.id = a.fk_totem
-  ) t on f.id = t.fk_filial
-  where f.fk_empresa = ${idEmpresa}
-  and f.id = ${idFilial}
-  group by f.id;
+    SELECT f.nome AS filial_nome, DAY(a.data_hora) AS dia, COUNT(a.id) AS qtd_alertas, p.nome AS promocao_nome FROM filial f 
+    LEFT JOIN empresa e ON e.id = f.fk_empresa
+    LEFT JOIN totem t ON f.id = t.fk_filial
+    LEFT JOIN promocao p ON f.id = p.fk_filial  
+    LEFT JOIN alerta a ON t.id = a.fk_totem
+    WHERE a.data_hora >= TIMESTAMP("${anoMes}-01") 
+    AND a.data_hora < TIMESTAMP("${anoMes}-01") + INTERVAL 1 MONTH
+    AND f.id = ${idFilial} 
+    AND e.id = ${idEmpresa}
+    AND p.id = ${idPromocao}
+    GROUP BY dia
+    ORDER BY dia DESC;
 `;
 
     console.log("Execuntando a instrução SQL: \n" + instrucaoSql);
